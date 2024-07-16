5495d613c546f8b41232f17f8e61c44798d9fa22
Albeiro <albeiroespitiasierra@gmail.com>
2024-07-16 18:57:43 -0500

    feat: edit home title

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
       <div className="container-home">
         <div className="container-news">
           <div className="title-container-news">
-            <h1>Noticias</h1>
+            <h1>Noticias de Albeiro</h1>
             <p>Noticias destacadas de la semana</p>
           </div>
           <div className="card-news-content">
